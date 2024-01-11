1e5e9ac6939c8439bd83438536c52cfcce7f3d78
FiocChuck <chasf@google.com>
2024-01-11 12:09:40 -0500

    add gemini functionality

--- src/main.py
+++ src/main.py
@@ -7,19 +7,24 @@ def main(cloud_event):
     print("test event")
     data = cloud_event.data
 
-    event_id = cloud_event["id"]
-    event_type = cloud_event["type"]
+    # event_id = cloud_event["id"]
+    # event_type = cloud_event["type"]
 
-    bucket = data["bucket"]
+    # bucket = data["bucket"]
     name = data["name"]
-    metageneration = data["metageneration"]
-    timeCreated = data["timeCreated"]
-    updated = data["updated"]
+    # metageneration = data["metageneration"]
+    # timeCreated = data["timeCreated"]
+    # updated = data["updated"]
 
-    print(f"Event ID: {event_id}")
-    print(f"Event type: {event_type}")
-    print(f"Bucket: {bucket}")
+    # print(f"Event ID: {event_id}")
+    # print(f"Event type: {event_type}")
+    # print(f"Bucket: {bucket}")
     print(f"File: {name}")
-    print(f"Metageneration: {metageneration}")
-    print(f"Created: {timeCreated}")
-    print(f"Updated: {updated}")
+    # print(f"Metageneration: {metageneration}")
+    # print(f"Created: {timeCreated}")
+    # print(f"Updated: {updated}")
+
+    project = "cf-data-analytics"
+    loc = "us-central1"
+
+    generate_text(project, loc, name)
