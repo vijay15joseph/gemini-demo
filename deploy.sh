e8a5125c7b7fd28a878d6036991d8a17adb8fd83
FiocChuck <chasf@google.com>
2024-02-06 11:13:17 -0500

    update sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-gcloud storage buckets create gs://gemini-cloud-function-source
+# gcloud storage buckets create gs://gemini-cloud-function-source
 
-gcloud storage buckets create gs://gemini-demo-images
-gsutil iam ch allUsers:objectViewer gs://twillio-images
+gcloud storage buckets create gs://gemini-demo-images --location=us-central1
+gsutil iam ch allUsers:objectViewer gs://gemini-demo-images
