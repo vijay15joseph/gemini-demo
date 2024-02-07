32b3ea29b49d3bf5de65e00aa080afa8454f8154
FiocChuck <chasf@google.com>
2024-02-06 20:55:00 -0500

    improve deployment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
 # gcloud storage buckets create gs://gemini-cloud-function-source
 
 gcloud storage buckets create gs://gemini-demo-images --location=us-central1
-gsutil iam ch allUsers:objectViewer gs://gemini-demo-images
+# gsutil iam ch allUsers:objectViewer gs://gemini-demo-images
+
+# change to a using gcloud storage
+gcloud storage buckets add-iam-policy-binding gs://gemini-demo-images --member=allUsers --role=roles/storage.objectViewer
+
+
+gcloud storage buckets create gs://cf-gemini-demo-app-host --location=us-central1
+gcloud storage buckets add-iam-policy-binding gs://cf-gemini-demo-app-host --member=allUsers --role=roles/storage.objectViewer
+gcloud storage cp web/ gs://cf-gemini-demo-app-host -r
