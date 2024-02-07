669523361d25957d21b4727e44ce579091af61e3
FiocChuck <chasf@google.com>
2024-02-07 16:25:33 -0500

    yaml bug

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 # gcloud storage buckets create gs://gemini-cloud-function-source
 
-gcloud storage buckets create gs://${{secrets.GENERATE_SOURCE_GCS}}
-# gsutil iam ch allUsers:objectViewer gs://gemini-demo-images
-
-# change to a using gcloud storage
-gcloud storage buckets add-iam-policy-binding gs://gemini-demo-images --member=allUsers --role=roles/storage.objectViewer
+gcloud storage buckets create gs://gemini-demo-images --location=us-central1; gcloud storage buckets add-iam-policy-binding gs://gemini-demo-images --member=allUsers --role=roles/storage.objectViewer
 
 
 gcloud storage buckets create gs://cf-gemini-demo-app-host --location=us-central1
