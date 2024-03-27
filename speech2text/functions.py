ef53d316575305a79e30aefd26e8ec610a6af65e
Vijay John-Britto <vijayjohnb@google.com>
2024-03-27 11:36:41 -0400

    Added the speec2text functionality, with gemini pro 1.0, store the audio file in cloud storage, cloud function will trigger the speech2text to transcribe and summarize it.

---
+++ speech2text/functions.py
@@ -0,0 +1,94 @@
+import vertexai
+from vertexai.preview.generative_models import GenerativeModel, Part
+from google.cloud import speech
+import vertexai.preview.generative_models as generative_models
+import firebase_admin
+from firebase_admin import firestore
+
+
+def generate_speech2text(project_id: str, location: str, file: str) -> str:
+    # Initiate Git Actions
+    # Initialize Vertex AI
+    vertexai.init(project=project_id, location=location)
+    # Initialize Speech Client
+    # Instantiates a client
+    # For more info on installing and using the client library, visit
+    # https://cloud.google.com/speech-to-text/docs/reference/libraries
+    client = speech.SpeechClient()
+
+    #Load the audio file from the Cloud Storage bucket
+    file = "gs://" + file
+
+    # The name of the audio file to transcribe
+    # audio_file = "resources/Google_Gnome.wav"
+
+    # Load the audio file into memory
+    with open(audio_file, "rb") as audio_file:
+        audio_content = audio_file.read()
+
+    # transcribe speech
+    audio = speech.RecognitionAudio(content=audio_content)
+
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=24000,
+        language_code="en-US",
+        model="default",
+        audio_channel_count=1,
+        enable_word_confidence=True,
+        enable_word_time_offsets=True,
+    )
+
+    # Detects speech in the audio file
+    operation = client.long_running_recognize(config=config, audio=audio)
+
+    print("Waiting for operation to complete...")
+    response = operation.result(timeout=90)
+
+    for result in response.results:
+        print("Transcript: {}".format(result.alternatives[0].transcript))
+
+   # Summarize the speech-to-text transcript using a generative AI model
+    summary = summarize(response.text)
+   
+    
+    # The response's audio_content is binary.
+    return summary
+
+
+def save_results(uuid, time, desc, url):
+    # import firebase_admin
+
+    # Application Default credentials are automatically created.
+    # app = firebase_admin.initialize_app()
+
+    if not firebase_admin._apps:
+        firebase_admin.initialize_app()
+
+    db = firestore.client()
+
+    doc_ref = db.collection("gemini-demo-speech2text-summary").document(uuid)
+    doc_ref.set({"timeStamp": time, "transcription": """""", "summary": desc, "speechUrl": url})
+
+
+def summarize(text : str):
+    vertexai.init(project="vijay-gcp-demo-project", location="us-central1")
+    model = GenerativeModel("gemini-1.0-pro-001")
+    responses = model.generate_content(
+        text+"""
+    Provide a summary of the above text in 2 sentences""",
+        generation_config={
+            "max_output_tokens": 2048,
+            "temperature": 0.9,
+            "top_p": 1
+        },
+        safety_settings={
+            generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+        stream=True,
+    )
+
+    return responses.text
---
+++ speech2text/main.py
@@ -0,0 +1,28 @@
+import functions_framework
+from functions import *
+import os
+
+
+@functions_framework.cloud_event
+def main(cloud_event):
+    # trigger data
+    data = cloud_event.data
+
+    bucket = data["bucket"]  # bucket
+    name = data["name"]  # file name
+    timeCreated = data["timeCreated"]  # time when file was created
+
+    project = os.getenv('PROJECT')
+    loc = os.getenv('LOCATION')
+
+    # project = "cf-data-analytics"  # required to initialize vertex client
+    # loc = "us-central1"
+    path = "gs://" + bucket + "/" + name  # uri
+
+    path_url = "https://storage.googleapis.com/" + bucket + "/" + name  # public url
+
+    # function that calls gemini api
+    output = generate_text(project, loc, path)
+
+    # save gemini output as document to firestore
+    save_results(name, timeCreated, output, path_url)
